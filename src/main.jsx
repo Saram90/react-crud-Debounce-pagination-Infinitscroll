9ce90da969460f869cb84c0fa18f7e5bcf1c879a
SaramRehman <saaramrehman@gmail.com>
2026-07-03 15:41:57 +0500

    version.json version add

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+async function checkVersion() {
+
+  const response = await fetch(
+    "/version.json?t=" + Date.now(),
+    { cache: "no-store" }
+  );
+
+  const data = await response.json();
+
+  const currentVersion =
+    localStorage.getItem("app_version");
+
+  if (!currentVersion) {
+
+    localStorage.setItem(
+      "app_version",
+      data.version
+    );
+
+    return;
+  }
+}
+
+checkVersion();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
